f9c9c20ec08f0835f485578d6f1affc0fa24ec53
Justin <justinmathiue0@gmail.com>
2023-05-24 22:54:53 -0700

    work on lab 08

---
+++ src/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500');
+const notFound = require('./error-handlers/404');
+const authRoutes = require('./middleware/auth/routes');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
